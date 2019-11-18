b7824bdedec191ed0325be45d9249c0fa499187d
Niklas Lausch <support@niki2k1.de>
2019-11-18 09:13:13 +0100

    Replaced Example Page with Bootstrap Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">Lightainer</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Documentaion</Nav.Link>
+        </Nav>
+        <Button variant="outline-secondary" href="https://github.com/Lightainer-Project/Lightainer" target="_blank"><FontAwesomeIcon icon={ faGithub } /> Github</Button>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
