887871357a979fb7beabaa40082ed48d4ce16ae2
Niklas Lausch <support@niki2k1.de>
2019-11-23 21:24:51 +0100

    Added Logo Showcase Added Textbox Added Navbar animation Fixed some Styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <Fragment>
-      <div className="navigation">
+      <div id="nav" className="navigation">
         <div className="container">
           <a href="/website">
             <img src="logo512.png" alt="Logo"></img>
@@ -15,7 +15,7 @@ function App() {
           <nav>
             <ul>
               <li><a href="#documentation">Documentation</a></li>
-              <li><a href="https://github.com/Lightainer-Project/Lightainer">Github</a></li>
+              <li><a href="https://github.com/Lightainer-Project/Lightainer" target="_blank" rel="noopener noreferrer">Github</a></li>
             </ul>
           </nav>
         </div>
@@ -24,25 +24,33 @@ function App() {
         <div className="container">
           <h1>Manage and Deploy <br /> Services</h1>
           <p>With Lightainer you can easily maintain, monitor <br />and configure your server.</p>
-          <button className="button">Get started</button>
+          <a className="button" href="/documentation#getstarted">Get started</a>
         </div>
       </div>
       <div className="cards">
         <div className="card primary shadow">
           <h1>Demo</h1>
           <p>Check out Lightainer in our Demo</p>
-          <button className="button">Demo</button>
+          <a className="button" href="/demo" target="_blank" rel="noopener noreferrer">Demo</a>
         </div>
         <div className="card dark shadow">
           <h1>Modules</h1>
           <p>Take a look in our Marketplace to <br />check out other Modules you <br />can use with Lightainer</p>
-          <button className="button">Marketplace</button>
+          <a className="button" href="/marketplace" target="_blank" rel="noopener noreferrer">Marketplace</a>
         </div>
         <div className="card secondary shadow">
           <h1>Support</h1>
           <p>You got a problem with<br />Lightainer? Create a Issue<br />on Github an we can help</p>
-          <button className="button">Issues</button>
+          <a className="button" href="https://github.com/Lightainer-Project/Lightainer/issues" target="_blank" rel="noopener noreferrer">Issues</a>
         </div>
+      </div>
+      <div className="showcase">
+        <a href="http://frozencloud.de/" target="_blank" rel="noopener noreferrer"><img src="frozencloud.png" alt="frozencloud logo"/></a>
+        <a href="https://kabema-consulting.de/" target="_blank" rel="noopener noreferrer"><img src="kabema.png" alt="kabema logo"/></a>
+      </div>
+      <div className="textbox dark">
+        <h1></h1>
+        <p></p>
       </div>
     </Fragment>
   );
